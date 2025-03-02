cabdc09d2e2ce9d5fac1df5581bf0e11dd1e2fbf
artemgunter <pscardo@mail.ru>
2025-03-02 15:06:41 +0700

    Create parol.py

---
+++ parol.py
@@ -0,0 +1,17 @@
+import random
+
+# Переменная с возможными символами для пароля
+symbols = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+# Запрос длины пароля у пользователя
+password_length = int(input("Введите длину пароля: "))
+
+# Переменная для хранения сгенерированного пароля
+generated_password = ""
+
+# Генерация пароля
+for _ in range(password_length):
+    generated_password += random.choice(symbols)
+
+# Вывод сгенерированного пароля
+print("Сгенерированный пароль:", generated_password)
